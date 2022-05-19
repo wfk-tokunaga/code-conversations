3a1bffe50aee2df3116a44f67625adbe04d64e57
wfk-tokunaga <wktokunaga@gmail.com>
2022-05-19 11:56:59 -0700

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS code_convo_db;
+
+CREATE DATABASE code_convo_db;
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const sequelize = require('./config/connection');
+
+// need to create our connection to the mysql server
+const app = express();
+const PORT = process.env.PORT || 3007;
+
+//Setting up middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Connect the express server 
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
